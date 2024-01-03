524f1bdc0640bf059ffac0277c24bc19073b1f51
Xun Li <lixun910@gmail.com>
2024-01-03 16:53:30 -0700

    pin loaders.gl schema to latest version

--- src/processors/src/data-processor.ts
+++ src/processors/src/data-processor.ts
@@ -383,6 +383,7 @@ export function processKeplerglDataset(
  * @returns dataset containing `fields` and `rows` or null
  */
 export function processArrowTable(arrowTable: ArrowTable): ProcessorResult | null {
+  // @ts-ignore - ArrowTable is not typed
   return processArrowBatches(arrowTable.data.batches);
 }
 
